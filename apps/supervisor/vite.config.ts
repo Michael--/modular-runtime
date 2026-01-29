67c3ac40b0a39b0b70eaff8b411e499c172a7000
M.Rieck <mr@number10.de>
2026-01-29 16:30:34 +0100

    build: configure vite buile

---
+++ apps/supervisor/vite.config.ts
@@ -0,0 +1,19 @@
+import { builtinModules } from 'node:module'
+import { defineConfig } from 'vite'
+
+const builtins = [...builtinModules, ...builtinModules.map((moduleName) => `node:${moduleName}`)]
+
+export default defineConfig({
+  build: {
+    target: 'node18',
+    lib: {
+      entry: 'src/supervisor.ts',
+      formats: ['es'],
+      fileName: 'supervisor',
+    },
+    sourcemap: true,
+    rollupOptions: {
+      external: [...new Set([...builtins, 'js-yaml'])],
+    },
+  },
+})
