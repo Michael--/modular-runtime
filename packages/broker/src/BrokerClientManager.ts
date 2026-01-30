11dbabfd1b3686594c4f5a997c6da97572c14648
M.Rieck <mr@number10.de>
2026-01-30 15:12:20 +0100

    feat: add BrokerClientManager, generic ClientManager

---
+++ packages/broker/src/BrokerClientManager.ts
@@ -0,0 +1,191 @@
+/* eslint-disable no-console */
+import * as grpc from '@grpc/grpc-js'
+import {
+  BrokerServiceClient,
+  NotifyServiceChangesResponse,
+  RegisterServiceRequest,
+} from '../../proto/generated/ts/broker/v1/broker'
+import { ClientManager } from './ClientManager'
+
+/**
+ * Interface representing extended service information.
+ */
+export interface ExtServiceInfo {
+  name: string
+  role: string
+  url: string
+  port: number
+}
+
+/**
+ * Class representing a manager for BrokerClient.
+ * It extends the ClientManager class to manage BrokerClient connections and service registrations.
+ */
+export class BrokerClientManager extends ClientManager<BrokerServiceClient> {
+  public static instance: BrokerClientManager | null = null
+  private registeredServices: ExtServiceInfo[] = []
+  public onChanges?: (changes: NotifyServiceChangesResponse) => void
+  private clientStream: grpc.ClientReadableStream<unknown> | null = null
+
+  /**
+   * Creates a singleton instance of BrokerClientManager.
+   * @param {string} url - The URL of the gRPC server.
+   * @param {number} port - The port of the gRPC server.
+   * @returns {BrokerClientManager} The singleton instance of BrokerClientManager.
+   */
+  public static create(url: string, port: number): BrokerClientManager {
+    if (BrokerClientManager.instance) return BrokerClientManager.instance
+    BrokerClientManager.instance = new BrokerClientManager(url, port)
+    return BrokerClientManager.instance
+  }
+
+  /**
+   * Private constructor to enforce singleton pattern.
+   * @param {string} url - The URL of the gRPC server.
+   * @param {number} port - The port of the gRPC server.
+   */
+  private constructor(url: string, port: number) {
+    super(url, port, BrokerServiceClient.serviceName, BrokerServiceClient)
+  }
+
+  /**
+   * Gets the client stream for service change notifications.
+   * @returns {grpc.ClientReadableStream<NotifyServiceChangesResponse>} The client stream.
+   * @throws {Error} If the client is not connected.
+   */
+  getClientStream(): grpc.ClientReadableStream<NotifyServiceChangesResponse> {
+    if (this.client == null) throw new Error('Client not connected')
+    return this.client.notifyServiceChanges({})
+  }
+
+  override connected() {
+    super.connected()
+    if (this.client == null) throw new Error('Client not connected')
+    this.clientStream = this.client.notifyServiceChanges({})
+    this.clientStream.on('data', (changes) => {
+      this.onChanges?.(changes)
+    })
+    this.clientStream.on('error', (error) => {
+      console.error('Client stream error:', error.message)
+      this.client = null
+    })
+    this.clientStream.on('end', () => {
+      console.log('Client stream ended')
+      this.client = null
+    })
+  }
+
+  override disconnected() {
+    super.disconnected()
+  }
+
+  /**
+   * Gets the available services from the broker.
+   * @returns {Promise<RegisterServiceRequest[]>} A promise that resolves to the list of available services.
+   */
+  public getAvailableServices(): Promise<RegisterServiceRequest[]> {
+    return new Promise((resolve, reject) => {
+      this.client?.getAvailableServices({}, (error, response) => {
+        if (error) {
+          reject(new Error('Failed to get available services'))
+        } else {
+          resolve(response.services)
+        }
+      })
+    })
+  }
+
+  /**
+   * Gets the service information for a specific gRPC client and role.
+   * @param {new (...args: any[]): grpc.Client} grpcClient - The gRPC client constructor.
+   * @param {string} [role="default"] - The role of the service.
+   * @returns {Promise<{ url: string; port: number } | null>} The service information or null if not found.
+   */
+  public async getService(
+    grpcClient: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      new (...args: any[]): grpc.Client
+      serviceName: string
+    },
+    role: string = 'default'
+  ) {
+    const clients = await this.getAvailableServices()
+    const service = clients.find(
+      (service) =>
+        service.info?.interfaceName === grpcClient.serviceName && service.info?.role === role
+    )
+    if (service != null) return { url: service.url, port: service.port }
+    return null
+  }
+
+  /**
+   * Registers a service with the broker.
+   * @param {new (...args: any[]): grpc.Client} grpcClient - The gRPC client constructor.
+   * @param {string} url - The URL of the service.
+   * @param {number} port - The port of the service.
+   */
+  public registerService(
+    grpcClient: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      new (...args: any[]): grpc.Client
+      serviceName: string
+    },
+    url: string,
+    port: number
+  ) {
+    const info: ExtServiceInfo = {
+      name: grpcClient.serviceName,
+      role: 'default',
+      url,
+      port,
+    }
+    this.client?.registerService(
+      {
+        info: {
+          interfaceName: info.name,
+          role: info.role,
+        },
+        url: info.url,
+        port: info.port,
+      },
+      (error) => {
+        if (error) {
+          console.error('Failed to register service:', info.name)
+        } else {
+          this.registeredServices.push(info)
+          console.log('Service registered successfully:', info.name)
+        }
+      }
+    )
+  }
+
+  /**
+   * Unregisters a service from the broker.
+   * @param {ExtServiceInfo} info - The extended service information.
+   * @returns {Promise<void>} A promise that resolves when the service is unregistered.
+   * @private
+   */
+  private async unregisterService(info: ExtServiceInfo): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client?.unregisterService({ interfaceName: info.name, role: info.role }, (error) => {
+        if (error) {
+          console.error(`Failed to unregister service ${JSON.stringify(info)}:`)
+          reject(error)
+        } else {
+          console.log(`Service ${JSON.stringify(info)} unregistered successfully:`)
+          resolve()
+        }
+      })
+    })
+  }
+
+  /**
+   * Shuts down the BrokerClientManager by unregistering all registered services.
+   * @returns {Promise<void>} A promise that resolves when all services are unregistered.
+   */
+  public async shutdown() {
+    const promises = this.registeredServices.map((service) => this.unregisterService(service))
+    this.registeredServices = []
+    await Promise.all(promises)
+  }
+}
---
+++ packages/broker/src/ClientManager.ts
@@ -0,0 +1,123 @@
+/* eslint-disable no-console */
+import * as grpc from '@grpc/grpc-js'
+import { credentials } from '@grpc/grpc-js'
+
+/**
+ * Abstract class representing a manager for gRPC clients.
+ * It provides functionality to connect to a gRPC server and handle service changes.
+ * It also provides functionality to handle connection and disconnection events.
+ * Reconnection is scheduled if the connection is lost.
+ * @template TClient - The type of the gRPC client.
+ * @template TServiceChangeNotification - The type of the service change notification.
+ */
+export abstract class ClientManager<TClient extends grpc.Client> {
+  private readonly address: string
+  public readonly serviceName: string
+  public client: TClient | null = null
+  public onConnected?: () => void
+  public onDisconnected?: () => void
+  private reconnectDelay: number = 3000 // 3 seconds
+  private deadlineDelay: number = 10000 // 10 seconds
+
+  /**
+   * Creates an instance of ClientManager.
+   * @param {string} url - The URL of the gRPC server.
+   * @param {number} port - The port of the gRPC server.
+   * @param {new (address: string, credentials: grpc.ChannelCredentials) => TClient} clientConstructor - The constructor for the gRPC client.
+   */
+  public constructor(
+    url: string,
+    port: number,
+    serviceName: string,
+    private clientConstructor: new (
+      address: string,
+      credentials: grpc.ChannelCredentials
+    ) => TClient
+  ) {
+    this.address = `${url}:${port}`
+    this.serviceName = serviceName
+    console.log(
+      `Creating ${this.serviceName} client manager at ${this.address}, reconnect delay: ${this.reconnectDelay / 1000}s, deadline delay: ${
+        this.deadlineDelay / 1000
+      }s`
+    )
+    this.scheduleReconnect()
+  }
+
+  /**
+   * Internal method to handle connection event.
+   * If needed, it can be overridden in the derived class.
+   */
+  protected connected() {}
+
+  /**
+   * Internal method to handle disconnection event.
+   * If needed, it can be overridden in the derived class.
+   */
+  protected disconnected() {}
+
+  /**
+   * Connects to the gRPC server and sets up the client and client stream.
+   * If the connection fails, it schedules a reconnect.
+   * @private
+   */
+  private connect() {
+    try {
+      if (this.client != null) return
+      this.client = new this.clientConstructor(this.address, credentials.createInsecure())
+
+      // deadline is 10 seconds in the future
+      this.client.waitForReady(new Date(Date.now() + this.deadlineDelay), (error) => {
+        if (error) {
+          console.error(`waitForReady: ${error.message}`)
+          this.client = null
+          this.scheduleReconnect()
+        } else {
+          console.log('waitForReady: done')
+          if (this.client == null) throw new Error('Client not connected')
+          // call internal connect and also notify any listeners
+          this.connected()
+          this.onConnected?.()
+
+          // TODO: fiddle connectivity state, ensure it is READY, assign number to enum value
+          const s = this.client.getChannel().getConnectivityState(true)
+          console.log('Connectivity state:', s)
+          if (s === undefined) {
+            console.error('Connectivity state is undefined')
+            this.client = null
+            this.scheduleReconnect()
+            return
+          }
+          // deadline is 2147483 seconds in the future, it is 24.8 days (will cause a reconnect at least once a month)
+          this.client
+            .getChannel()
+            .watchConnectivityState(s, new Date(Date.now() + 2147483 * 1000), (error) => {
+              const s2 = this.client?.getChannel().getConnectivityState(true)
+              console.warn('Watch connectivity state:', s, s2, error?.message)
+              if ((s === 2 && s2 !== 2) /*READY*/ || error) {
+                this.client = null
+                this.scheduleReconnect()
+              }
+            })
+        }
+      })
+    } catch (_error) {
+      this.client = null
+      console.error('A Failed to create broker client:', _error)
+      this.scheduleReconnect()
+    }
+  }
+
+  /**
+   * Schedules a reconnect attempt after a delay.
+   * @private
+   */
+  private scheduleReconnect() {
+    if (this.client != null) return
+    console.log(`Reconnecting in ${this.reconnectDelay / 1000} seconds...`)
+    setTimeout(() => {
+      console.log(`Reconnecting ${this.serviceName} at ${this.address} now...`)
+      this.connect()
+    }, this.reconnectDelay)
+  }
+}
