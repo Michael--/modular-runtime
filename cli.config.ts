5277a402a1b08ba40c076f037a2c30f19e5c80a9
M.Rieck <mr@number10.de>
2026-02-07 14:07:50 +0100

    ci: add ci-runner-cli

---
+++ cli.config.ts
@@ -0,0 +1,55 @@
+import type { CiRunnerConfig } from '@number10/ci-runner-cli/types'
+
+const config = {
+  output: {
+    format: 'pretty',
+    verbose: false,
+  },
+  steps: [
+    {
+      id: 'clean',
+      name: 'Clean',
+      command: 'pnpm run clean',
+      optional: true,
+    },
+    {
+      id: 'gen',
+      name: 'Generate',
+      command: 'pnpm run gen',
+    },
+    {
+      id: 'build',
+      name: 'Build',
+      command: 'pnpm run build',
+    },
+    {
+      id: 'typecheck',
+      name: 'Typecheck',
+      command: 'pnpm run typecheck',
+    },
+    {
+      id: 'lint',
+      name: 'Lint',
+      command: 'pnpm run lint',
+    },
+    {
+      id: 'integration-tests',
+      name: 'Integration Tests',
+      command: 'pnpm run test:integration',
+      optional: true,
+    },
+    {
+      id: 'unit-tests',
+      name: 'Unit Tests',
+      command: 'pnpm run test',
+    },
+    {
+      id: 'e2e-tests',
+      name: 'E2E Tests',
+      command: 'pnpm run test:e2e',
+      optional: true,
+    },
+  ],
+} satisfies CiRunnerConfig
+
+export default config
